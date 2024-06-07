1c5feb0c115383a843f3a4297fe7069ce2c92d4a
Carolous Ghobriall <carolous.ghobrial@gmail.com>
2024-06-07 14:28:48 -0500

    try

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || "3000";
 const announcements = require("./routes/announcments");
 const users = require("./routes/users");
 const calendar = require("./routes/calendar");
