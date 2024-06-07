852e50e5bf25bc2fed1bc06c064060da7c0991b3
Carolous Ghobriall <carolous.ghobrial@gmail.com>
2024-06-07 14:24:26 -0500

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 const announcements = require("./routes/announcments");
 const users = require("./routes/users");
 const calendar = require("./routes/calendar");
